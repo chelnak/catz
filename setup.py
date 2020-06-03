9a61329821017719ce98ce3b5178c494a04f98c3
Craig Gumbley <craiggumbley@gmail.com>
2020-06-03 22:22:02 +0100

    use cx_freeze

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+    "includes": [
+        "jcat/utilities"
+    ],
+    "packages": [
+        "pyfiglet"
+    ]
+}
+
+bdist_msi_options = {}
+
+exe = Executable(
+    script="jcat/app.py",
+    targetName="jcat.exe"
+)
+
+version = os.environ.get('GITVERSION_SEMVER).', '0.0-local')
+
+setup(
+    name="jcat",
+    version=version,
+    description="A colourful syntax highlighting tool for your terminal.",
+    options={
+        "build_exe": build_exe_options,
+        'bdist_msi': bdist_msi_options},
+    executables=[exe]
+)
