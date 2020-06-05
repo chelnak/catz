c42a619f82ad3e16a992cad394b6a8b716db2f57
Craig Gumbley <craiggumbley@gmail.com>
2020-06-05 10:29:49 +0100

    install script

---
+++ install.ps1
@@ -0,0 +1,11 @@
+$JcatPath = "$ENV:USERPROFILE\AppData\Local\Programs\jcat"
+$ENV:PATH="$ENV:PATH;$JcatPath"
+
+$LatestRelease = Invoke-RestMethod -Method Get -Uri "https://api.github.com/repos/chelnak/jcat/releases/latest"
+$LatestAsset = $LatestRelease.assets.Where{$_.name -Like "*.msi"}
+
+Invoke-RestMethod -Method Get -Uri $LatestAsset.browser_download_url -OutFile $ENV:TEMP/$LatestAsset.name -FollowRelLink
+
+& msiexec /i $ENV:TEMP\$LatestAsset.name /passive
+
+Write-Warning -Message "$JcatPath needs to be added to the PATH environment variable"
