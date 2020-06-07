bcec53cd0d80c61281ce02aaf274f8a11739a0a2
Craig Gumbley <craiggumbley@gmail.com>
2020-06-07 20:24:10 +0100

    Move setup back to root

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import find_packages, setup
+
+PRODUCT_NAME = 'jcat'
+
+DEPENDENCIES = []
+
+VERSION = os.environ.get('GITVERSION_SEMVER', '0.0.0')
+
+setup(
+    name=PRODUCT_NAME,
+    version=VERSION,
+    author='chelnak',
+    author_email='chelnak@github',
+    description='A colourful syntax highlighting tool for your terminal.',
+    url='https://github.com/chelnak/jcat',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': ['jcat = jcat.app:cli']
+    },
+    install_requires=DEPENDENCIES
+)
